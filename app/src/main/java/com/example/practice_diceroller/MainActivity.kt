5f268e74fd75ce721bf7cb1999adf0fdb161bc54
Ray <bill56523@gmail.com>
2023-07-15 23:43:32 +0800

    2023.07.15 update by Ray.

--- app/src/main/java/com/example/practice_diceroller/MainActivity.kt
+++ app/src/main/java/com/example/practice_diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.practice_diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +14,9 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+
+        // Do a dice roll when the app starts
+        rollDice()
     }
 
     private fun rollDice() {
@@ -23,11 +26,37 @@ class MainActivity : AppCompatActivity() {
         val diceRoll = dice.roll()
         val diceRoll2 = dice2.roll()
 
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
 
-        val resultTextView2: TextView = findViewById(R.id.textView2)
-        resultTextView2.text = diceRoll2.toString()
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val drawableResource2 = when (diceRoll2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
+
+        diceImage2.setImageResource(drawableResource2)
+        diceImage2.contentDescription = diceRoll2.toString()
+//        val resultTextView: TextView = findViewById(R.id.textView)
+//        resultTextView.text = diceRoll.toString()
+
+//        val resultTextView2: TextView = findViewById(R.id.textView2)
+//        resultTextView2.text = diceRoll2.toString()
     }
 }
 
